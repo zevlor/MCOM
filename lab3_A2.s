9307b4081dd18cba5ce027030436fe716987f242
zevlor <112320770+zevlor@users.noreply.github.com>
2023-06-13 04:05:41 +0200

    Update lab3_A2.s

--- lab3_A2.s
+++ lab3_A2.s
@@ -1,21 +1,24 @@
+# Input: r4 - Delay value (number of clock cycles)
 init_timer:
-  movi r5, 0xFF202008   ; Adresse des Registers periodl
-  str r4, [r5]          ; Verzögerungswert in periodl schreiben
-  
-  movi r5, 0xFF20200C   ; Adresse des Registers periodh
-  str r4, [r5]          ; Verzögerungswert in periodh schreiben
-  
-  ret
+    movia r16, 0xFF202004   # Load the address of the control register into r16
+    movia r17, 0xFF202000   # Load the address of the status register into r17
+    movia r18, 0xFF202008   # Load the address of the periodl register into r18
+    movia r19, 0xFF20200C   # Load the address of the periodh register into r19
+
+    andi r20, r4, 0xFFFF    # Mask out the upper 16 bits of the delay value (lower 16 bits remain)
+    srli r21, r4, 16        # Shift right the delay value by 16 bits (to extract the upper 16 bits)
+
+    stw r20, (r18)          # Store the lower 16 bits of the delay value to periodl register
+    stw r21, (r19)          # Store the upper 16 bits of the delay value to periodh register
+
+    movi r22, 2             # Set the START bit (bit 2) in the control register
+    stw r22, (r16)          # Start the timer by writing to the control register
+    ret
 
 wait_timer:
-  movi r5, 0xFF202004   ; Adresse des Registers control
-  
-  movi r6, 2            ; Wert 2 für das Start-Bit
-  str r6, [r5]          ; Timer starten
-  
-  loop:
-    ldr r7, [r5]        ; Control-Register auslesen
-    andi r7, r7, 0x1    ; Nur das RUN-Bit überprüfen
-    beq loop            ; Warten, bis das RUN-Bit auf 0 gesetzt ist
-  
-  ret
+    ldw r23, (r17)          # Read the value of the status register
+    andi r23, r23, 1        # Mask out all bits except the TO bit (bit 0)
+    beq r23, r0, wait_timer # Repeat the loop if the TO bit is not set
+
+    ret
+
